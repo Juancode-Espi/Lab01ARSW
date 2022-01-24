243a9a5f2dbec6e17a9e2e99d104a07f508887db
unknown <juan.pico-m@labinfo.is.escuelaing.edu.co>
2022-01-24 17:09:35 -0500

    lo intente jajaj

---
+++ PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH-master/src/main/java/edu/eci/arsw/blacklistvalidator/ServerSearch.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.blacklistvalidator;
+import java.lang.Thread;
+import java.util.List;
+public class ServerSearch extends Thread {
+    public void run(){
+
+    }
+
+    public ServerSearch(int inicial, int fin,String lista ){
+        
+
+    }
+}
